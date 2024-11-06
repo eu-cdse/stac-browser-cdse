9c58f5befca480b0e0acd1eb102f9fb48fb7ae65
Matthias Mohr <webmaster@mamo-net.de>
2024-11-06 21:25:07 +0100

    Configure and style for CDSE

--- assetActions.config.js
+++ assetActions.config.js
@@ -1,20 +1,2 @@
-//import Felt from './src/actions/assets/Felt.js';
-import Cesium from './src/actions/assets/Cesium.js';
-import CopcViewer from './src/actions/assets/CopcViewer.js';
-import F3D from './src/actions/assets/F3D.js';
-import GeoJsonIo from './src/actions/assets/GeoJsonIo.js';
-// import Geofox from './src/actions/assets/Geofox.js';
-// import Geoparquet from './src/actions/assets/Geoparquet.js';
-// import Potree from './src/actions/assets/Potree.js';
-import Protomaps from './src/actions/assets/Protomaps.js';
-
 export default {
-  //Felt
-  Cesium,
-  CopcViewer,
-  F3D,
-  GeoJsonIo,
-  // Geoparquet, // not ready yet
-  // Potree,
-  Protomaps,
 };
