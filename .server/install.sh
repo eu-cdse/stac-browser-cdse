07d328ae1875bb0eb1d4972a43e470457a7a9959
Matthias Mohr <webmaster@mamo-net.de>
2025-02-25 14:13:48 +0100

    Add cron for certbot to install guide

--- .server/install.sh
+++ .server/install.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install -y nodejs npm apache2 snapd
+sudo apt install -y nodejs npm apache2 snapd cron
 sudo snap install --classic certbot
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
 cd ~
@@ -8,3 +8,7 @@ sudo a2enmod rewrite
 cp ./stac-browser-cdse/.server/browser.conf /etc/apache2/sites-available/000-default.conf
 sudo certbot --apache -d browser.stac.dataspace.copernicus.eu
 sudo service apache2 restart
+sudo crontab -e
+# in ctrontab, add the following lines (without the leading '# '):
+# 0 3 * * * /usr/bin/certbot renew --apache -n
+# 0 4 * * * /bin/systemctl restart apache2
