3918b55b0333b409871f1bd1219ee0b16155f9cc
Matthias Mohr <m.mohr@moregeo.it>
2026-09-09 19:22:07 +0200

    Fix install/update scripts

--- .server/install.sh
+++ .server/install.sh
@@ -14,7 +14,7 @@ git checkout opensearch
 
 # Configure Apache and SSL
 sudo a2enmod rewrite
-cp ~/stac-browser-cdse/.server/browser.conf /etc/apache2/sites-available/000-default.conf
+sudo cp ~/stac-browser-cdse/.server/browser.conf /etc/apache2/sites-available/000-default.conf
 sudo apache2ctl configtest
 sudo service apache2 restart
 
