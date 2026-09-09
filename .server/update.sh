c466f689d0f2c490b5afd955ede3ac34501b79e0
Matthias Mohr <m.mohr@moregeo.it>
2026-09-09 14:06:54 +0200

    Add featured widget, other updates

--- .server/update.sh
+++ .server/update.sh
@@ -1,7 +1,10 @@
 cd ~/stac-browser-cdse
+# Update the Browser
 git pull
 npm install
+# Build the Browser
 npm run build
+# Copy the built files to the Apache web server directory
 sudo rm -r /var/www/html/**
 sudo cp dist/* /var/www/html -r
 sudo chown -R "$USER":www-data /var/www/html
